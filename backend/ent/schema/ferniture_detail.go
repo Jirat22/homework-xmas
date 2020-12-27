6f0eb6f550828297b36b2c269162db2966cfad88
Jirat22 <jiratz2208@gmail.com>
2020-12-27 15:31:54 +0700

    Create furniture_detail Entity - close #3

---
+++ backend/ent/schema/ferniture_detail.go
@@ -0,0 +1,18 @@
+package schema
+
+import "github.com/facebookincubator/ent"
+
+// Ferniture_Detail holds the schema definition for the Ferniture_Detail entity.
+type Ferniture_Detail struct {
+	ent.Schema
+}
+
+// Fields of the Ferniture_Detail.
+func (Ferniture_Detail) Fields() []ent.Field {
+	return nil
+}
+
+// Edges of the Ferniture_Detail.
+func (Ferniture_Detail) Edges() []ent.Edge {
+	return nil
+}
